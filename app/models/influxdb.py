dca361ddc84e25c7d5683d5c3f4010cca6434f2e
Clément VALENTIN <clement.valentin.partner@decathlon.com>
2022-12-03 01:36:44 +0100

    Add other method to import data in influxdb

--- app/models/influxdb.py
+++ app/models/influxdb.py
@@ -1,20 +1,22 @@
 import datetime
 
+import __main__ as app
+
 import influxdb_client
-from dateutil.relativedelta import relativedelta
 from dateutil.tz import tzlocal
 from influxdb_client.client.util import date_utils
 from influxdb_client.client.util.date_utils import DateHelper
 from influxdb_client.client.write_api import ASYNCHRONOUS, SYNCHRONOUS
 
-import __main__ as app
 from dependencies import *
-from models.log import Log
 
 
 class InfluxDB:
 
-    def __init__(self, hostname, port, token, org="myelectricaldata.fr", bucket="myelectricaldata", write_options="SYNCHRONOUS"):
+    def __init__(self, hostname, port, token, org="myelectricaldata.fr", bucket="myelectricaldata", method="batching",
+                 write_options=None):
+        if write_options is None:
+            write_options = {}
         self.hostname = hostname
         self.port = port
         self.token = token
@@ -23,7 +25,40 @@ class InfluxDB:
         self.influxdb = {}
         self.write_api = {}
         self.delete_api = {}
-        self.write_options = write_options
+        self.method = method
+        self.write_options = {}
+        if "batch_size" in write_options:
+            self.write_options["batch_size"] = write_options["batch_size"]
+        else:
+            self.write_options["batch_size"] = 1000
+        if "flush_interval" in write_options:
+            self.write_options["flush_interval"] = write_options["flush_interval"]
+        else:
+            self.write_options["flush_interval"] = 1000
+        if "jitter_interval" in write_options:
+            self.write_options["jitter_interval"] = write_options["jitter_interval"]
+        else:
+            self.write_options["jitter_interval"] = 0
+        if "retry_interval" in write_options:
+            self.write_options["retry_interval"] = write_options["retry_interval"]
+        else:
+            self.write_options["retry_interval"] = 5000
+        if "max_retry_time" in write_options:
+            self.write_options["max_retry_time"] = write_options["max_retry_time"]
+        else:
+            self.write_options["max_retry_time"] = "180_000"
+        if "max_retries" in write_options:
+            self.write_options["max_retries"] = write_options["max_retries"]
+        else:
+            self.write_options["max_retries"] = 5
+        if "max_retry_delay" in write_options:
+            self.write_options["max_retry_delay"] = write_options["max_retry_delay"]
+        else:
+            self.write_options["max_retry_delay"] = 125_000
+        if "exponential_base" in write_options:
+            self.write_options["exponential_base"] = write_options["exponential_base"]
+        else:
+            self.write_options["exponential_base"] = 2
         self.connect()
 
     def connect(self):
@@ -47,10 +82,19 @@ class InfluxDB:
                 "https://github.com/m4dm4rtig4n/enedisgateway2mqtt#configuration-file"
             ])
 
-        if self.write_options == "ASYNCHRONOUS":
+        if self.method.upper() == "ASYNCHRONOUS":
             self.write_api = self.influxdb.write_api(write_options=ASYNCHRONOUS)
-        else:
+        elif self.method.upper() == "SYNCHRONOUS":
             self.write_api = self.influxdb.write_api(write_options=SYNCHRONOUS)
+        else:
+            self.write_api = self.influxdb.write_api(write_options=influxdb_client.WriteOptions(
+                batch_size=self.write_options["batch_size"],
+                flush_interval=self.write_options["flush_interval"],
+                jitter_interval=self.write_options["jitter_interval"],
+                retry_interval=self.write_options["retry_interval"],
+                max_retries=self.write_options["max_retries"],
+                max_retry_delay=self.write_options["max_retry_delay"],
+                exponential_base=self.write_options["exponential_base"]))
         self.delete_api = self.influxdb.delete_api()
 
     def purge_influxdb(self):
