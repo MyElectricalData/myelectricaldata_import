ccf7fbe761503b7aa3f7a11e79483f025f1f5052
Clément VALENTIN <clement.valentin.partner@decathlon.com>
2023-08-19 11:43:02 +0200

    Update swagger documentation

--- app/doc.py
+++ app/doc.py
@@ -3,4 +3,30 @@ DOCUMENTATION = {
     "measurement_direction": "consumption or production",
     "begin": "Date de début format : 2023-07-21",
     "end": "Date de fin format : 2023-07-21",
+    "date_format": "Date format : 2023-07-21",
+    "target_full": """Valeur possible :
+    
+    - tempo
+    - ecowatt
+    - account_status
+    - contract
+    - addresses
+    - consumption
+    - consumption_detail
+    - production
+    - production_detail
+    - consumption_max_power
+    - stat
+    - mqtt
+    - home_assistant
+    - influxdb
+    """,
+    "target": """Valeur possible :
+    
+    - consumption
+    - consumption_detail
+    - production
+    - production_detail
+    - consumption_max_power
+    """,
 }
