dfa61c254b91ccb8e06ef87bb7a7e957eb05b894
Clément VALENTIN <clement.valentin.partner@decathlon.com>
2023-12-22 09:44:33 +0100

    clean

--- app/templates/usage_point.py
+++ app/templates/usage_point.py
@@ -858,24 +858,6 @@ class UsagePoint:
                         price_tempo = round(value_tempo, 2)
                     html += "<tr>"
                     html += f"<td class='table_recap_header'>{years}</td>"
-                    # evolution_1 = round(price_hchp - price_base, 2)
-                    # evolution_2 = round(price_tempo - price_base, 2)
-                    # color_1 = "green"
-                    # color_2 = "green"
-                    # if price_base < price_hchp:
-                    #     color_1 = "red"
-                    #     evolution_1 = f"+{evolution_1}"
-                    # if price_tempo and price_base < price_tempo:
-                    #     color_2 = "red"
-                    #     evolution_2 = f"+{evolution_2}"
-                    # text_color = "var(--text-color);"
-                    # if color_1 == "red" and color_2 == "red":
-                    #     text_color = "rgb(16, 150, 24);"
-                    # html += f"<td>" \
-                    #         f"<div style='float: left; width: 50%; padding-top: 14px;'><b style='font-size: 18px; color: {text_color}'>{price_base} €</b></div>" \
-                    #         f"<div style='float: right; width: 50%'><span style='color: {color_1}; font-size: 12px'>HC/HP : {evolution_1}€</span><br>" \
-                    #         f"<span style='color: {color_2}; font-size: 12px'>Tempo : {evolution_2}€</span></div>" \
-                    #         f"</td>"
                     html += generate_price_compare(
                         {"price_1": price_base, "price_2": price_hchp, "price_3": price_tempo, "lib_1": "HC/HP",
                          "lib_2": "Tempo"})
@@ -885,54 +867,8 @@ class UsagePoint:
                     html += generate_price_compare(
                         {"price_1": price_tempo, "price_2": price_base, "price_3": price_hchp, "lib_1": "Base",
                          "lib_2": "HC/HP"})
-                    # evolution_1 = round(price_base - price_hchp, 2)
-                    # evolution_2 = round(price_tempo - price_hchp, 2)
-                    # color_1 = "green"
-                    # color_2 = "green"
-                    # if price_hchp < price_base:
-                    #     color_1 = "red"
-                    #     evolution_1 = f"+{evolution_1}"
-                    # if price_tempo and price_hchp < price_tempo:
-                    #     color_2 = "red"
-                    #     evolution_2 = f"+{evolution_2}"
-                    # text_color = "var(--text-color);"
-                    # if color_1 == "red" and color_2 == "red":
-                    #     text_color = "rgb(16, 150, 24);"
-                    # html += f"<td>" \
-                    #         f"<div style='float: left; width: 50%; padding-top: 14px;'><b style='font-size: 18px; color: {text_color}'>{price_hchp} €</b></div>" \
-                    #         f"<div style='float: right; width: 50%'><span style='color: {color_1}; font-size: 12px'>Base : {evolution_1}€</span><br>" \
-                    #         f"<span style='color: {color_2}; font-size: 12px'>Tempo : {evolution_2}€</span></div>" \
-                    #         f"</td>"
-                    # if price_tempo:
-                    #     evolution_1 = round(price_base - price_tempo, 2)
-                    #     evolution_2 = round(price_hchp - price_tempo, 2)
-                    #     color_1 = "green"
-                    #     color_2 = "green"
-                    #     if price_tempo < price_base:
-                    #         color_1 = "red"
-                    #         evolution_1 = f"+{evolution_1}"
-                    #     if price_tempo and price_tempo < price_hchp:
-                    #         color_2 = "red"
-                    #         evolution_2 = f"+{evolution_2}"
-                    #     text_color = "var(--text-color);"
-                    #     if color_1 == "red" and color_2 == "red":
-                    #         text_color = "rgb(16, 150, 24);"
-                    #     html += f"<td>" \
-                    #             f"<div style='float: left; width: 50%; padding-top: 14px;'><b style='font-size: 18px; color: {text_color}'>{price_tempo} €</b></div>" \
-                    #             f"<div style='float: right; width: 50%'><span style='color: {color_1}; font-size: 12px'>Base : {evolution_1}€</span><br>" \
-                    #             f"<span style='color: {color_2}; font-size: 12px'>HC/HP : {evolution_2}€</span></div>" \
-                    #             f"</td>"
 
             html += "</table>"
-            # html += """
-            # <table style='border: none;'>
-            #         <tr>
-            #             <td style='width: 30px; background-color: green'>&nbsp;</td>
-            #             <td style='border: none'>Gain&nbsp;&nbsp;</td>
-            #             <td style='width: 30px; background-color: #FF0000'>&nbsp;</td>
-            #             <td style='border: none'>Perte</td>
-            #         </tr>
-            # </table>"""
         return html
 
     def recap(self, data):
--- tests/TESTING.md
+++
@@ -1,8 +0,0 @@
-### Tests Unitaires
-
-Pour executer les tests unitaires de ce projet, veuillez vous placer a la racine du projet, et executez la commande
-suivante:
-
-```commandline
-python -m pytest
-```
