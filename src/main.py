5615745c67a8b951b61627d93cef9db8b0360730
Clément VALENTIN <clement.valentin.partner@decathlon.com>
2024-01-22 22:47:29 +0100

    feat: add semantic release

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from routers import data
 from routers import html
 from routers import info
 
-__version__ = "0.9.4"
+VERSION = "0.9.4"
 
 if "DEV" in environ or "DEBUG" in environ:
     title_warning("Run in Development mode")
