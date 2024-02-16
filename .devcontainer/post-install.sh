0374f0b889ecdfd39a7c52f6cf181930daf8a86f
Clément VALENTIN <clement.valentin.partner@decathlon.com>
2024-02-16 17:43:43 +0100

    fix: test

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -19,3 +19,16 @@ echo ""
 echo ""
 echo "Install environment"
 make install
+
+eval "$(ssh-agent -s)"
+cat >> ~/.bashrc <<- EOM
+if [ -z "\$SSH_AUTH_SOCK" ]; then
+   # Check for a currently running instance of the agent
+   RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+   if [ "\$RUNNING_AGENT" = "0" ]; then
+        # Launch a new instance of the agent
+        ssh-agent -s &> \$HOME/.ssh/ssh-agent
+   fi
+   eval `cat \$HOME/.ssh/ssh-agent`
+fi
+EOM
